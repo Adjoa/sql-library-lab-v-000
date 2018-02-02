9d835b7046e1ff891bfa521e35e59931a01fb13d
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-02 19:12:36 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -9,19 +9,19 @@ CREATE TABLE series (
 --   id INTEGER PRIMARY KEY,
 --   name TEXT
 -- );
--- 
+--
 -- CREATE TABLE authors (
 --   id INTEGER PRIMARY KEY,
 --   name TEXT,
 -- );
--- 
+--
 -- CREATE TABLE books (
 --   id INTEGER PRIMARY KEY,
 --   title TEXT,
 --   year INTEGER,
 --   series_id INTEGER
 -- );
--- 
+--
 -- CREATE TABLE characters (
 --   id INTEGER PRIMARY KEY,
 --   name TEXT,
@@ -30,7 +30,7 @@ CREATE TABLE series (
 --   series_id INTEGER,
 --   author_id INTEGER
 -- );
--- 
+--
 -- CREATE TABLE character_books (
 --   id INTEGER PRIMARY KEY,
 --   book_id INTEGER,
